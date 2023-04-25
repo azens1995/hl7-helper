4da1ce36263a1e6ee698d4bb24f8a291e113ee39
sajidantechie <sajidansari33272@gmail.com>
2023-04-25 10:11:18 +0545

    Complete hl7 parsing

---
+++ src/parser/utils/parser.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const hl7v2 = require('@redoxengine/redox-hl7-v2');
+const parser = require('@rimiti/hl7-object-parser');
+const segmentMapping = require("./../segment.mapping.json")
+
+class Hl7Parser {
+    #filePath = null;
+
+    constructor(filePath) {
+        this.#filePath = filePath;
+        
+    }
+    
+    parse() {
+        if(!this.#checkIfFileExist()) {
+            throw new Error("FIle not found.")
+        }
+        
+        const hl7Message = fs.readFileSync(this.#filePath, 'utf-8');
+        const decodedMessage = parser.decode(hl7Message.replace(/\n/g, "\r"), segmentMapping);
+        return decodedMessage;
+    }
+
+    #checkIfFileExist() {
+        if(!this.#filePath) {
+            return false
+        }
+
+        return fs.existsSync(this.#filePath)
+    }
+}
+
+module.exports = Hl7Parser;
