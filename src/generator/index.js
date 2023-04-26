669bf9897da0c474edb39e1f0268ab133f48c8dc
Ishwar Gautam <ishwargautam1@gmail.com>
2023-04-26 15:28:52 +0545

    Create separate function for exporting hl7 message.

--- src/generator/index.js
+++ src/generator/index.js
@@ -1,15 +1,9 @@
 const hl7v2 = require("@redoxengine/redox-hl7-v2");
 const { ADT_A01 } = require("./messages/adt_a01");
+const { exportHL7Message } = require("./utils/exportHL7Message");
 
 const generator = new hl7v2.Generator();
 const data = generator.write(ADT_A01);
 
 // Export generated message to the file
-const fs = require("fs");
-const { generateFileName } = require("./utils/generateName");
-
-const fileName = generateFileName("ADT_A01");
-
-const writeStream = fs.createWriteStream(`results/${fileName}`);
-writeStream.write(data);
-writeStream.end();
+exportHL7Message(data, "ADT_A01");
