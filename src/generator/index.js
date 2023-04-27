ef644e73cd591a2a1a0db5edf89d7d60b0f980ac
Ishwar Gautam <ishwargautam1@gmail.com>
2023-04-27 17:06:31 +0545

    Add ORU_R01 message.

--- src/generator/index.js
+++ src/generator/index.js
@@ -1,10 +1,13 @@
 const { ADT_A01 } = require("./messages/adt_a01");
+const { ORU_R01 } = require("./messages/oru_r01");
 const { MESSAGE } = require("./constants/message");
 const hl7v2 = require("@redoxengine/redox-hl7-v2");
 const { exportHL7Message } = require("./utils/exportHL7Message");
 
 const generator = new hl7v2.Generator();
-const data = generator.write(ADT_A01);
+const data1 = generator.write(ADT_A01);
+const data2 = generator.write(ORU_R01);
 
 // Export generated message to the file
-exportHL7Message(data, MESSAGE.ADT_A01);
+exportHL7Message(data1, MESSAGE.ADT_A01);
+exportHL7Message(data2, MESSAGE.ORU_R01);
