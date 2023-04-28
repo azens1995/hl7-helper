3ac749a7f983ac024f5444e8d4a6a7a0ef43a455
sajidantechie <sajidansari33272@gmail.com>
2023-04-28 14:49:36 +0545

    Remove sample folder and add orders folder

--- src/parser/index.js
+++ src/parser/index.js
@@ -3,7 +3,7 @@ const Hl7Parser = require("./utils/parser");
 const reportExtraction = require("./utils/exportReport");
 const exportData = require("./utils/exportData");
 
-const sampleFilePath = path.join(__dirname, "./../../Hl7Samples/ADT_AO1.txt");
+const sampleFilePath = path.join(__dirname, "./../../orders/ADT_AO1.txt");
 const parser = new Hl7Parser(sampleFilePath);
 const decodedMessage = parser.parse();
 const messageType = decodedMessage.MSH[9][1] + "_" + decodedMessage.MSH[9][2];
