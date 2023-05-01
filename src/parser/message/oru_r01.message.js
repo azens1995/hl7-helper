fb159f251b80fbb370bd03358b629e7a6456df9f
sajidantechie <sajidansari33272@gmail.com>
2023-05-01 12:14:03 +0545

    Implement fatory pattern for getting type of message and add util to store global value

--- src/parser/message/oru_r01.message.js
+++ src/parser/message/oru_r01.message.js
@@ -1,56 +1,61 @@
+const path = require("path");
+const Hl7Parser = require("../utils/parser");
 const { PID } = require("../constant/app.constant");
 
-function oruR01Message(data) {
-  const messageHeaderInfo = data.MSH;
-  const patientInfo = data[PID.PATIENT_RESULT][0][PID.PATIENT][PID.PID_INFO];
-  const orderObservationInfo =
-    data[PID.PATIENT_RESULT][0][PID.ORDER_OBSERVATION][0][PID.OBSERVATION];
-  const messageHeader = mapMSH(messageHeaderInfo);
-  const patient = mapPID(patientInfo);
-  const observation = mapOrderObservation(orderObservationInfo);
-  return {
-    messageHeader,
-    patient,
-    observation,
-  };
-}
+class OruR01Message {
+  constructor() {
+    const sampleFilePath = path.join(__dirname, "../../../orders/ORU_R01.txt");
+    const parser = new Hl7Parser(sampleFilePath);
+    const decodedMessage = parser.parse();
+    const messageHeaderInfo = decodedMessage.MSH;
+    const patientInfo =
+      decodedMessage[PID.PATIENT_RESULT][0][PID.PATIENT][PID.PID_INFO];
+    const orderObservationInfo =
+      decodedMessage[PID.PATIENT_RESULT][0][PID.ORDER_OBSERVATION][0][
+        PID.OBSERVATION
+      ];
+    this.messageHeader = this.#mapMSH(messageHeaderInfo);
+    this.patient = this.#mapPID(patientInfo);
+    this.observation = this.#mapOrderObservation(orderObservationInfo);
+  }
 
-function mapMSH(mshMessageData) {
-  return {
-    senderApplication: mshMessageData[3][1],
-    senderFacility: mshMessageData[4][1],
-    senderDate: mshMessageData[7][1],
-    messageType: mshMessageData[9][1] + "_" + mshMessageData[9][2],
-  };
-}
+  #mapMSH(mshMessageData) {
+    return {
+      senderApplication: mshMessageData[3][1],
+      senderFacility: mshMessageData[4][1],
+      senderDate: mshMessageData[7][1],
+      messageType: mshMessageData[9][1] + "_" + mshMessageData[9][2],
+    };
+  }
 
-function mapPID(pidMessageData) {
-  return {
-    fullName: pidMessageData[5][0][2] + pidMessageData[5][0][1][1],
-    doBirth: pidMessageData[7][1],
-    patientId: pidMessageData[3][0][1],
-    mrnNumber: pidMessageData[3][0][5],
-    address: {
-      street: pidMessageData[11][0][1][1],
-      city: pidMessageData[11][0][3],
-      state: pidMessageData[11][0][4],
-    },
-    sex: pidMessageData[8],
-  };
-}
+  #mapPID(pidMessageData) {
+    return {
+      fullName: pidMessageData[5][0][2] + pidMessageData[5][0][1][1],
+      doBirth: pidMessageData[7][1],
+      patientId: pidMessageData[3][0][1],
+      mrnNumber: pidMessageData[3][0][5],
+      address: {
+        street: pidMessageData[11][0][1][1],
+        city: pidMessageData[11][0][3],
+        state: pidMessageData[11][0][4],
+      },
+      sex: pidMessageData[8],
+    };
+  }
 
-function mapOrderObservation(orderObservationData) {
-  const observationData = [];
-  orderObservationData.forEach((observation) => {
-    const data = observation.OBX[5][0].split("^");
-    observationData.push({
-      encodingType: data[3],
-      typeOfData: data[1],
-      data: data[4],
-      resultStatus: observation.OBX[11],
+  #mapOrderObservation(orderObservationData) {
+    const observationData = [];
+    orderObservationData.forEach((observation) => {
+      const data = observation.OBX[5][0].split("^");
+      observationData.push({
+        encodingType: data[3],
+        typeOfData: data[1],
+        data: data[4],
+        resultStatus: observation.OBX[11],
+      });
     });
-  });
-  return observationData;
+    return observationData;
+  }
 }
 
-module.exports = oruR01Message;
+module.exports = OruR01Message;
