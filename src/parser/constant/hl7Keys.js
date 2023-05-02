f34d5d3e36ec5b26a120af4cf72bd5222f34fc0a
sajidantechie <sajidansari33272@gmail.com>
2023-05-02 10:40:55 +0545

    HL7-012: Refactor to use map for storing parsed message object

---
+++ src/parser/constant/hl7Keys.js
@@ -0,0 +1,24 @@
+const keys = {
+  FULL_NAME: "fullName",
+  DOB_BIRTH: "dobBirth",
+  PHONE_NUMBER: "phoneNumber",
+  EMAIL: "email",
+  PATIENT_ID: "patientId",
+  MRN_NUMBER: "mrnNumber",
+  STREET: "street",
+  CITY: "city",
+  STATE: "state",
+  MARITAL_STATUS: "maritalStatus",
+  SEX: "sex",
+  SSN_NUMBER: "ssnNumber",
+  SENDER_APPLICATION: "senderApplication",
+  SENDER_FACILITY: "senderFacility",
+  SENDER_DATE: "senderDate",
+  MESSAGE_TYPE: "messageType",
+  ENCODING_TYPE: "encodingType",
+  TYPE_OF_DATA: "typeOfData",
+  DATA: "data",
+  RESULT_STATUS: "resultStatus",
+};
+
+module.exports = keys;
--- src/parser/index.js
+++ src/parser/index.js
@@ -2,12 +2,14 @@ const store = require("./utils/store");
 const exportReport = require("./utils/exportReport");
 const Hl7MessageFactory = require("./factory/Hl7factory");
 const messageType = require("./constant/messageType.constant");
+const { getAdtA01Keys, getOruR01Keys } = require("./factory/hl7Keys");
 
+//call api to retrieve required keys and store to global state for future uses during parsing.
+store.set(messageType.ADT_A01, getAdtA01Keys());
+store.set(messageType.ORU_R01, getOruR01Keys());
 const Hl7factory = new Hl7MessageFactory();
 const adtA01Message = Hl7factory.getMessage(messageType.ADT_A01);
 const oruR01Message = Hl7factory.getMessage(messageType.ORU_R01);
-store.set(messageType.ADT_A01, adtA01Message);
-store.set(messageType.ORU_R01, oruR01Message);
 console.log("ADT_AO1", adtA01Message);
 console.log("ORU_R01", oruR01Message);
 exportReport();
