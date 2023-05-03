924adbc6a9b3fda0be8dfc110e103c027f19cab1
Sajid Ansari <sajidansari33272@gmail.com>
2023-05-03 21:30:07 +0545

    Add feature to merge cover page to report

--- src/parser/index.js
+++ src/parser/index.js
@@ -1,4 +1,3 @@
-const fs = require("fs");
 const path = require("path");
 const store = require("./utils/store");
 const Hl7Parser = require("./utils/parser");
@@ -6,25 +5,21 @@ const exportReport = require("./utils/exportReport");
 const Hl7MessageFactory = require("./factory/Hl7factory");
 const { getCurrentDateInYYYYMMDD } = require("./utils/date");
 const messageType = require("./constant/messageType.constant");
+const { getFile, getFilesFromDir } = require("./utils/file");
 const { getAdtA01Keys, getOruR01Keys } = require("./factory/hl7Keys");
 const Hl7FilesPath = path.join(
   __dirname,
   "./../../orders",
   getCurrentDateInYYYYMMDD()
 );
-
-const path1 = path.join(__dirname, "./../../cover-page/coverpage.txt");
-const base64CoverPagePdf = fs.readFileSync(path1, "utf-8");
+const coverPagePath = path.join(__dirname, "./../../cover-page/coverpage.txt");
+const coverPage = getFile(coverPagePath);
+const files = getFilesFromDir(Hl7FilesPath);
 
 //call api to retrieve required keys and store to global state for future uses during parsing.
 store.set(messageType.ADT_A01, getAdtA01Keys());
 store.set(messageType.ORU_R01, getOruR01Keys());
 const Hl7factory = new Hl7MessageFactory();
-const files = fs.readdirSync(Hl7FilesPath, { encoding: "utf-8" });
-if (!files.length) {
-  throw new Error("Files not found.");
-}
-
 const reports = [];
 
 for (const file of files) {
@@ -33,7 +28,7 @@ for (const file of files) {
   const messageHeader = decodedMessage.MSH;
   const fileType = messageHeader[9][1] + "_" + messageHeader[9][2];
   const message = Hl7factory.getMessage(fileType.toUpperCase(), decodedMessage);
-  reports.push(exportReport(message, base64CoverPagePdf));
+  reports.push(exportReport(message, coverPage));
   console.log(fileType, message);
 }
 
