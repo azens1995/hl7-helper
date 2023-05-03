c23ea8ad73be999173a920a28d93ce994e9f479c
sajidantechie <sajidansari33272@gmail.com>
2023-05-03 15:01:09 +0545

    Add pdf-lib library

--- src/parser/index.js
+++ src/parser/index.js
@@ -7,25 +7,36 @@ const Hl7MessageFactory = require("./factory/Hl7factory");
 const { getCurrentDateInYYYYMMDD } = require("./utils/date");
 const messageType = require("./constant/messageType.constant");
 const { getAdtA01Keys, getOruR01Keys } = require("./factory/hl7Keys");
-const basePath = path.join(__dirname, "./../../orders");
-const todayDate = getCurrentDateInYYYYMMDD();
+const Hl7FilesPath = path.join(
+  __dirname,
+  "./../../orders",
+  getCurrentDateInYYYYMMDD()
+);
+
+const path1 = path.join(__dirname, "./../../cover-page/coverpage.txt");
+const base64CoverPagePdf = fs.readFileSync(path1, "utf-8");
 
 //call api to retrieve required keys and store to global state for future uses during parsing.
 store.set(messageType.ADT_A01, getAdtA01Keys());
 store.set(messageType.ORU_R01, getOruR01Keys());
 const Hl7factory = new Hl7MessageFactory();
-const sampleFilePath = `${basePath}/${todayDate}`;
-const files = fs.readdirSync(sampleFilePath, { encoding: "utf-8" });
+const files = fs.readdirSync(Hl7FilesPath, { encoding: "utf-8" });
 if (!files.length) {
   throw new Error("Files not found.");
 }
 
-files.forEach((file) => {
-  const parser = new Hl7Parser(`${sampleFilePath}/${file}`);
+const reports = [];
+
+for (const file of files) {
+  const parser = new Hl7Parser(`${Hl7FilesPath}/${file}`);
   const decodedMessage = parser.parse();
   const messageHeader = decodedMessage.MSH;
   const fileType = messageHeader[9][1] + "_" + messageHeader[9][2];
   const message = Hl7factory.getMessage(fileType.toUpperCase(), decodedMessage);
-  exportReport(message);
+  reports.push(exportReport(message, base64CoverPagePdf));
   console.log(fileType, message);
+}
+
+Promise.all(reports).then(() => {
+  console.log("Reports are generated SuccessFully........");
 });
--- src/parser/utils/exportReport.js
+++ src/parser/utils/exportReport.js
@@ -1,24 +1,79 @@
 const fs = require("fs");
-const { getCurrentDateInYYYYMMDD } = require("../utils/date");
+const path = require("path");
+const { PDFDocument } = require("pdf-lib");
 
-function exportReport(message) {
-  if (!message) {
+async function exportReport(message, base64CoverPagePdf) {
+  if (!message || !base64CoverPagePdf) {
     return;
   }
   const { observationData, messageType: mshType } = message;
   if (!observationData) {
     return;
   }
-  observationData.forEach(({ data: base64Encoded }, index) => {
-    const folderName = `reports/${mshType}-${getCurrentDateInYYYYMMDD()}`;
+
+  for (const value of observationData) {
+    const folderName = `reports/${mshType}`;
     if (!fs.existsSync(folderName)) {
       fs.mkdirSync(folderName);
     }
-    fs.writeFileSync(
-      `${folderName}/${new Date().getTime()}_${index + 1}.pdf`,
-      base64Encoded,
-      "base64"
-    );
-  });
+    const { data: base64PDF2 } = value;
+    const mergedBase64PDF = await mergePDFs(base64CoverPagePdf, base64PDF2);
+
+    try {
+      fs.writeFileSync(
+        `${folderName}/${new Date().getTime()}.pdf`,
+        mergedBase64PDF,
+        "base64"
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  }
 }
+
+async function mergePDFs(base64PDF1, base64PDF2) {
+  // Load the base64 encoded PDF files
+  const pdfDoc1 = await PDFDocument.load(Buffer.from(base64PDF1, "base64"));
+  const pdfDoc2 = await PDFDocument.load(Buffer.from(base64PDF2, "base64"));
+
+  // Create a new PDF document
+  const mergedPdfDoc = await PDFDocument.create();
+
+  // Add the pages from the first PDF
+  const pdfDoc1Pages = await mergedPdfDoc.copyPages(
+    pdfDoc1,
+    pdfDoc1.getPageIndices()
+  );
+  pdfDoc1Pages.forEach((page) => {
+    mergedPdfDoc.addPage(page);
+  });
+
+  // Add the pages from the second PDF
+  const pdfDoc2Pages = await mergedPdfDoc.copyPages(
+    pdfDoc2,
+    pdfDoc2.getPageIndices()
+  );
+  pdfDoc2Pages.forEach((page) => {
+    mergedPdfDoc.addPage(page);
+  });
+
+  // Save the merged PDF to a buffer
+  const mergedPdfBytes = await mergedPdfDoc.save();
+
+  // Convert the buffer to a base64 encoded string
+  const mergedBase64PDF = Buffer.from(mergedPdfBytes).toString("base64");
+
+  return mergedBase64PDF;
+}
+
+// const path1 = path.join(__dirname, "./../../../cover-page/coverpage.txt");
+
+// const path2 = path.join(__dirname, "./../../../cover-page/page.txt");
+// // Example usage
+// const base64PDF1 = fs.readFileSync(path1, "utf-8");
+// const base64PDF2 = fs.readFileSync(path2, "utf-8");
+// mergePDFs(base64PDF1, base64PDF2).then((result) => {
+//   console.log(result);
+// });
+
 module.exports = exportReport;
